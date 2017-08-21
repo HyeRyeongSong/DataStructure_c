867406e25e2a1ae549dadd0d819cf0e353bd1bf0
= <=>
2017-08-21 19:22:21 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/DataStructure_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure_c.dir/main.c.o"
+  "DataStructure_c.pdb"
+  "DataStructure_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DataStructure_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
