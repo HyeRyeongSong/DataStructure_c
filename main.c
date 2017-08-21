f3b2a7c009fec5b1d828ad4b582bd4b0d73d87e3
= <=>
2017-08-21 20:55:01 +0900

    수 찾기_2 (binary search)

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+int CompareValue(const void* a, const void* b);
+bool binarySearch(int* iArray, int iLength, int iTarget);
 
 int main()
 {
@@ -18,22 +20,44 @@ int main()
     scanf("%d", &M);
     iSearch = calloc(M, sizeof(int));
     bisExist = calloc(M, sizeof(bool));
+
+    qsort(iArray, N, sizeof(int), CompareValue);
+
     for(int i=0; i<M; ++i)
     {
         scanf("%d", &iSearch[i]);
+        bisExist[i] = binarySearch(iArray, N, iSearch[i]);
     }
 
     for(int i=0; i<M; ++i)
     {
-        for(int j=0; j<N; ++j)
-        {
-            if(iSearch[i] == iArray[j])
-            {
-                bisExist[i] = 1;
-                break;
-            }
-        }
         printf("%d\n", bisExist[i]);
     }
+    return 0;
+}
 
+bool binarySearch(int* iArray, int iLength, int iTarget)
+{
+    int iFirst = 0;
+    int iLast = iLength-1;
+    int iMid;
+    while(iFirst <= iLast)
+    {
+        iMid = (iFirst + iLast) / 2;
+        if(iArray[iMid] == iTarget)
+            return true;
+        else
+        {
+            if(iArray[iMid] < iTarget)
+                iFirst = iMid + 1;
+            else
+                iLast = iMid - 1;
+        }
+    }
+    return false;
+}
+
+int CompareValue(const void* a, const void* b)
+{
+    return ( *(int*)a - *(int*)b);
 }
