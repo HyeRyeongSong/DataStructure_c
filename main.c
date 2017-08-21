68db5ad05c296b9d9025526e1d70804e204f2e4f
= <=>
2017-08-21 19:51:27 +0900

    수 찾기_1

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{
+    int N;
+    int M;
+    int* iArray;
+    int* iSearch;
+    bool* bisExist;
+    scanf("%d", &N);
+    iArray = calloc(N, sizeof(int));
+    for(int i=0; i<N; ++i)
+    {
+        scanf("%d", &iArray[i]);
+    }
+    scanf("%d", &M);
+    iSearch = calloc(M, sizeof(int));
+    bisExist = calloc(M, sizeof(bool));
+    for(int i=0; i<M; ++i)
+    {
+        scanf("%d", &iSearch[i]);
+    }
+
+    for(int i=0; i<M; ++i)
+    {
+        for(int j=0; j<N; ++j)
+        {
+            if(iSearch[i] == iArray[j])
+            {
+                bisExist[i] = 1;
+                break;
+            }
+        }
+        printf("%d\n", bisExist[i]);
+    }
+
 }
