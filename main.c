115cfa4d187645de49372402679f3ff49115cbb4
= <=>
2017-08-27 17:18:58 +0900

    Fibonacci (Func)

--- main.c
+++ main.c
@@ -1,63 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-int CompareValue(const void* a, const void* b);
-bool binarySearch(int* iArray, int iLength, int iTarget);
+int Fibonacci(int iNum);
 
 int main()
 {
-    int N;
-    int M;
-    int* iArray;
-    int* iSearch;
-    bool* bisExist;
-    scanf("%d", &N);
-    iArray = calloc(N, sizeof(int));
-    for(int i=0; i<N; ++i)
+    for(int i=0; i<10; ++i)
     {
-        scanf("%d", &iArray[i]);
-    }
-    scanf("%d", &M);
-    iSearch = calloc(M, sizeof(int));
-    bisExist = calloc(M, sizeof(bool));
-
-    qsort(iArray, N, sizeof(int), CompareValue);
-
-    for(int i=0; i<M; ++i)
-    {
-        scanf("%d", &iSearch[i]);
-        bisExist[i] = binarySearch(iArray, N, iSearch[i]);
-    }
-
-    for(int i=0; i<M; ++i)
-    {
-        printf("%d\n", bisExist[i]);
+        printf("%d\t", Fibonacci(i+1));
     }
     return 0;
 }
 
-bool binarySearch(int* iArray, int iLength, int iTarget)
+int Fibonacci(int iNum)
 {
-    int iFirst = 0;
-    int iLast = iLength-1;
-    int iMid;
-    while(iFirst <= iLast)
-    {
-        iMid = (iFirst + iLast) / 2;
-        if(iArray[iMid] == iTarget)
-            return true;
-        else
-        {
-            if(iArray[iMid] < iTarget)
-                iFirst = iMid + 1;
-            else
-                iLast = iMid - 1;
-        }
-    }
-    return false;
-}
-
-int CompareValue(const void* a, const void* b)
-{
-    return ( *(int*)a - *(int*)b);
+    if(iNum == 1)
+        return 0;
+    else if(iNum ==2)
+        return 1;
+    else
+        return Fibonacci(iNum -1) + Fibonacci(iNum -2);
 }
