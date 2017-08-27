70e8cb308562ccaeec1e938a7d986ae0bc3770df
= <=>
2017-08-27 17:55:56 +0900

    Binary Search using Recursive Function

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-int Fibonacci(int iNum);
+
+int BSearchRecur(int* iArray, int iFirst, int iLast, const int iTarget);
 
 int main()
 {
-    for(int i=0; i<10; ++i)
-    {
-        printf("%d\t", Fibonacci(i+1));
-    }
+    int* iArray = calloc(10, sizeof(int));
+    for(int i= 0; i<10; ++i)
+        iArray[i] = i+1;
+    int iNum = BSearchRecur(iArray, 0, 9, 2);
+    if(iNum == -1)
+        printf("No target available\n");
+    else
+        printf("The index of the targeted number is %d\n", iNum);
     return 0;
 }
 
-int Fibonacci(int iNum)
+int BSearchRecur(int* iArray, int iFirst, int iLast, const int iTarget)
 {
-    if(iNum == 1)
-        return 0;
-    else if(iNum ==2)
-        return 1;
+    int iMid;
+    if(iFirst > iLast)
+        return -1;
+    iMid = (iFirst + iLast) / 2;
+    if (iArray[iMid] == iTarget)
+        return iMid;
+    else if (iArray[iMid] > iTarget)
+        return BSearchRecur(iArray, iFirst, iMid-1, iTarget);
     else
-        return Fibonacci(iNum -1) + Fibonacci(iNum -2);
+        return BSearchRecur(iArray, iMid+1, iLast, iTarget);
 }
+
+//오랜만에 코딩하더니 return문을 안쓰는 실수를 하다닛!!!
